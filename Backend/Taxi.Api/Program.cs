946787589ef0f47dcd29dcdd56267544aee3aef1
NidzaX <59400424+NidzaX@users.noreply.github.com>
2024-08-06 08:42:53 +0200

    Integrate the LoginUser functionality from C# backend into React
    Fronted Arc officially started :ambulance:

---
+++ Backend/Taxi.Api/Program.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Taxi.Application;
+using Taxi.Infrastructure;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "http://localhost:44319", //change port
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["tempsecret"]!))
+    };
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4000")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowReactApp");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Backend/Taxi.Domain/Rides/RideErrors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taxi.Domain.Abstractions;
+
+namespace Taxi.Domain.Rides
+{
+    public static class RideErrors
+    {
+        public static readonly Error NotFound = new(
+            "Ride.NotFound",
+            "The Ride with the specified identifier was not found");
+
+        public static readonly Error NotReserved = new(
+            "Ride.NotReserved",
+            "The Ride is not pending");
+
+        public static readonly Error NotConfirmed = new(
+            "Ride.NotReserved",
+            "The Ride is not confirmed");
+
+        public static readonly Error AlredyExists = new(
+           "Ride.AlredyExists",
+           "The Ride alredy exists");
+    }
+}
